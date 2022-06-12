fa367641e9b68f75935bd2e82a79c708ec462f87
Duwab <contact@superpaas.pro>
2022-06-12 10:52:22 +0200

    Create DB if not exists

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('express')();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -16,40 +16,53 @@ app.use(function(req, res, next) {
     next();
 });
 
+const initDb = () => {
+    return require('mysql2/promise').createConnection({
+        host     : process.env.DB_HOSTNAME,
+        user     : process.env.DB_USERNAME,
+        password : process.env.DB_PASSWORD
+    }).then((connection) => {
+        connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_DATABASE};`);
+    });
+}
 
-require('../database/orm')().then(models => {
-    app.models = models;
-    require('./src/strategies/local/routes')(app);
-    require('./src/strategies/github/routes')(app);
-    require('./src/jwt/routes')(app);
-    app.get('/users/:id', (req, res) => {
-        app.models.user.findByPk(req.params.id).then(user => {
-            if(!user) {
-                res.status(404).send({message: 'User does not exist'});
-            } else {
-                res.send(user.getPublicInfos());
-            }
+const initORM = () => {
+    require('../database/orm')().then(models => {
+        app.models = models;
+        require('./src/strategies/local/routes')(app);
+        require('./src/strategies/github/routes')(app);
+        require('./src/jwt/routes')(app);
+        app.get('/users/:id', (req, res) => {
+            app.models.user.findByPk(req.params.id).then(user => {
+                if(!user) {
+                    res.status(404).send({message: 'User does not exist'});
+                } else {
+                    res.send(user.getPublicInfos());
+                }
+            });
+        });
+        app.get('/resources_owners/:id', (req, res) => {
+            app.models.oauthResourceOwner.isOwnerRegistered(req.params.id, 'github').then(resourceOwner => {
+                //console.log(token.getUser);
+                res.send(resourceOwner);
+            }).catch(err => {
+                console.log('error', err);
+                res.status(500).send(err.message)
+            });
+        });
+        app.get('/mock-github-callback/:owner_id', (req, res) => {
+            app.models.user.createFromResourceOwner(req.params.owner_id, 'github', 'Duwab').then(user => {
+                //console.log(token.getUser);
+                res.send(app.jwt.sign(user));
+            }).catch(err => {
+                console.log('error', err);
+                res.status(500).send(err.message)
+            });
         });
     });
-    app.get('/resources_owners/:id', (req, res) => {
-        app.models.oauthResourceOwner.isOwnerRegistered(req.params.id, 'github').then(resourceOwner => {
-            //console.log(token.getUser);
-            res.send(resourceOwner);
-        }).catch(err => {
-            console.log('error', err);
-            res.status(500).send(err.message)
-        });
-    });
-    app.get('/mock-github-callback/:owner_id', (req, res) => {
-        app.models.user.createFromResourceOwner(req.params.owner_id, 'github', 'Duwab').then(user => {
-            //console.log(token.getUser);
-            res.send(app.jwt.sign(user));
-        }).catch(err => {
-            console.log('error', err);
-            res.status(500).send(err.message)
-        });
-    });
-});
+}
+
+initDb().then(initORM);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
